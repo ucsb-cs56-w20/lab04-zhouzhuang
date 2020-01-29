75cb66266469bfe74a29557c461c7d182e996e92
Joe Zhuang <zhouzhuang@umail.ucsb.edu>
2020-01-29 12:08:10 -0800

    jz - added controller method for multiplication results

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -90,7 +90,7 @@ public class OperationsController {
         logger.info("getSubtractResult ratCalcForm=" + ratCalcForm);
         ratCalcForm.setOp("-");
 
-        // TODO: Fill this in with appropriate code
+        // Fixed TODO
        if (!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)){
            Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
            Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
@@ -108,6 +108,24 @@ public class OperationsController {
     public String getMultiply(Model model) {
         RatCalcForm ratCalcForm = new RatCalcForm();
         ratCalcForm.setOp("x");
+        model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/multiply";
+    }
+
+    @GetMapping("/multiply/results")
+    public String getMultiplyResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult) {
+        logger.info("getMultiplyResult ratCalcForm=" + ratCalcForm);
+        ratCalcForm.setOp("*");
+
+        if (!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)){
+            Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
+            Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
+            Rational result = Rational.product(r1, r2);
+            logger.info("r1=" + r1 + " r2=" + r2 + " result=" + result);
+            ratCalcForm.setNumResult(result.getNumerator());
+            ratCalcForm.setDenomResult(result.getDenominator());
+        }
+
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/multiply";
     }
