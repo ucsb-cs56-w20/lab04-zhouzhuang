f18c0ca5fc1401c4e119a1eaa534408ed8d8ae64
Joe Zhuang <zhouzhuang@umail.ucsb.edu>
2020-01-29 12:13:07 -0800

    jz - fixed multiply operator

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -115,7 +115,7 @@ public class OperationsController {
     @GetMapping("/multiply/results")
     public String getMultiplyResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult) {
         logger.info("getMultiplyResult ratCalcForm=" + ratCalcForm);
-        ratCalcForm.setOp("X");
+        ratCalcForm.setOp("x");
 
         if (!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)){
             Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
